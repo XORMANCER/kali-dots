59c4608c6e4739fe89cf66550316079f69fa7627
xormancer <oberonx90@gmail.com>
2024-03-03 14:56:12 -0500

    Fixed nvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add alduin
+  { "sainnhe/gruvbox-material" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  }
+}
